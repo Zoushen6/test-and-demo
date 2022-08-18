8b14a82a5fecaf5b123f258d20c1ba28e173f488
祖士洋 <zushiyang@idataway.com>
2022-08-18 18:20:22 +0800

    object flatten

--- test.js
+++ test.js
@@ -16,8 +16,38 @@ let key = 'carLicensenumber'
 let a = {'carLicensenumber':'123465','name':'zhangsna'}
 let f = {'name':'123465','dddd':'zhangsna','carLicensenumber':'zhangsna',}
 f = Object.assign(a,f)
-console.log(f);
+// console.log(f);
 
 let d = a[key]
+
+
+let obj = {
+    a: {
+        b: 'hello ',
+        c: {
+            d: 'world'
+        }
+    },
+    e: 'hello world'
+}
+
+const fun = (obj) => {
+    let res = {}
+    const flat = (key,value) => {
+        if(typeof value == 'object') {
+            let arr = Object.keys(value);
+            for(let i of arr) {
+                flat(key+ '.'+ i,value[i])
+            }
+        }else {
+            res[key.slice(1)] = value
+            return ;
+        }
+    }
+    flat('',obj)
+    return res
+}
+
+console.log(fun(obj));
 
 
